175ed6a10aca684a791c31a072c8aa06c1aad34d
Fabricio <contact@fabricioguardia.com>
2020-12-21 21:31:43 -0800

    updated title

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
   return <>
     <Head>
       <>
-        <title>SPM Analyzer</title>
+        <title>SPM Analyzer - iOS and Android App for Amazon Sellers - Sales Estimator - Rank Calculator</title>
         <link rel="stylesheet" href="/fonts/fonts.css"/>
         <link rel="stylesheet" href="https://use.typekit.net/fgb2wcm.css"></link>
         <link rel="stylesheet" href="https://use.typekit.net/fgb2wcm.css"></link>

d20b1fbb9fb001401da6fd3bae1faaf9b8ee55b6
Fabricio <contact@fabricioguardia.com>
2021-01-13 14:55:00 -0800

    updated config

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const port = 3001
 
 app.use('/api/sales', async (req, res) => {
   const {market, category, rank} = req.body
-
   try {
     const response = await axios.get(`https://il7kji6jcl.execute-api.us-east-2.amazonaws.com/dev/salesEstimator?market=${encodeURIComponent(market)}&category=${encodeURIComponent(category)}&rank=${encodeURIComponent(rank)}`)
     console.log(response)

7fb582c4e1d1e06e05e26c05367a1bbabba0f123
Fabricio <contact@fabricioguardia.com>
2021-04-29 15:19:12 -0700

    price page url user parameters

--- pages/withUser.js
+++ pages/withUser.js
@@ -1,16 +1,29 @@
 import {API} from '../config'
 import axios from 'axios'
 import {getUser} from '../utils/user'
-import Router from 'next/router'
+import Cookies from 'cookies'
+axios.defaults.withCredentials = true
 
 const withUser = Page => {
     const WithAuthUser = props => <Page {...props} />
     WithAuthUser.getInitialProps = async (context)  => {
-      console.log(context.query)
+      const cookies = new Cookies(context.req, context.res)
+      const email = context.query.user
+      const password = context.query.token
       const user = getUser(context.req)
       let newUser = null
 
       if(user){newUser = user.split('=')[1]}
+
+      if(context.query.token){
+        try {
+          const responseLogin = await axios.post(`${API}/auth/login`, {email, password})
+          cookies.set('user', JSON.stringify(responseLogin.data))
+          newUser = JSON.stringify(responseLogin.data)
+        } catch (error) {
+          console.log(error)
+        }
+      }
 
       if(user == null){
           return {
